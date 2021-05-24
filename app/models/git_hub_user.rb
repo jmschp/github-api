7a21300a10ee281771189c33cb6c988e545edcc4
Miguel Pimenta <jmiguelpimenta@gmail.com>
2021-05-23 22:33:45 -0300

    feat: add model validations

--- app/models/git_hub_user.rb
+++ app/models/git_hub_user.rb
@@ -1,3 +1,8 @@
 class GitHubUser < ApplicationRecord
   has_many :repos
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :public_repos, presence: true, numericality: :only_integer
+  validates :followers, presence: true, numericality: :only_integer
+  validates :following, presence: true, numericality: :only_integer
 end
