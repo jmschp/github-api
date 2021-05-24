2c3110af4ebe9db208dad1b3105d20fa4176fcb7
Miguel Pimenta <jmiguelpimenta@gmail.com>
2021-05-24 10:10:10 -0300

    feat: change root page to git_hub_users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :git_hub_users, only: %i[index create]
+  root "git_hub_users#index"
+  resources :git_hub_users, only: %i[create]
 end
