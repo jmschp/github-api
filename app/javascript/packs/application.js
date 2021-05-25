a9e466f1df3fcbda581d7fc4135d9e9432827035
Miguel Pimenta <jmiguelpimenta@gmail.com>
2021-05-25 10:01:26 -0300

    feat: add bootstrap tooltips

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,17 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 
 import fetch_git_hub_api from "./fetch_git_hub_api";
+import { initTooltips } from "./init_bootstrap";
 
 document.addEventListener("turbolinks:load", () => {
   fetch_git_hub_api();
-})
+  initTooltips();
+});
---
+++ app/javascript/packs/init_bootstrap.js
@@ -0,0 +1,7 @@
+export function initTooltips() {
+  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+}
+
