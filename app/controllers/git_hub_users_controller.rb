dd982a6783cebb2127236b3bad46d49fbe85efbd
Miguel Pimenta <jmiguelpimenta@gmail.com>
2021-05-25 11:19:17 -0300

    style: changes to comply with rubocop

--- app/controllers/git_hub_users_controller.rb
+++ app/controllers/git_hub_users_controller.rb
@@ -5,8 +5,7 @@ class GitHubUsersController < ApplicationController
     @git_hub_users = GitHubUser.all
   end
 
-  def show
-  end
+  def show; end
 
   def create
     git_hub_user = GitHubUser.new(git_hub_user_params)
@@ -25,7 +24,7 @@ class GitHubUsersController < ApplicationController
   end
 
   def git_hub_user_params
-    git_hub_user_params = params.require(:username).permit(:login, :name, :avatar_url, :public_repos, :followers, :following, :created_at)
+    git_hub_user_params = params.require(:username).permit(:login, :name, :avatar_url, :public_repos, :followers, :following, :created_at) # rubocop:disable Layout/LineLength
     git_hub_user_params[:username] = git_hub_user_params.delete :login
     git_hub_user_params[:since] = git_hub_user_params.delete :created_at
     git_hub_user_params
